dfb980a0bdf5797e260615e678cc4f06e3cf80a3
lily2019github <lid1991cool@gmail.com>
2021-06-06 17:36:47 -0700

    update by Dan Li

--- src/components/User.js
+++ src/components/User.js
@@ -1,9 +1,8 @@
 /* 
-Author: Zhao Tang, Dan Li, Xiao Zhang, Yun Chang
+Author: Bochao, Zhao Tang, Dan Li, Xiao Zhang, Yun Chang
 Version 
     1.0 - 
     2.0 - 06/05/2021 - add features from non-login
-
 */
 import React, { Component } from 'react';
 import { bindActionCreators } from "redux";
@@ -31,22 +30,19 @@ class User extends Component {
     // test data, should get data from store latter
     // since delivered/estimate deliver time and other basic info are not stored together,
     // my current data structure is not accurate, we can change this part of code latter after discussion with backend
-    state = {
-        processing: [{ from: "local", to: "future", size: "50m^3m", orderWeight: "10 pounds", shipTime: "2021-05-21-12:21", deliverTime: "2021-05-27-12:21" },
-        { from: "company", to: "home", size: "100m^2m", orderWeight: "40 pounds", shipTime: "1999-05-21-12:21", deliverTime: "1999-05-23-12:21" },
-        { from: "china", to: "USA", size: "2000m^400m", orderWeight: "8 pounds", shipTime: "200-05-21-12:21", deliverTime: "1999-05-24-12:21" }],
-        delivered: [{ from: "santa clara", to: "san cruz", size: "50inch^3inch", orderWeight: "10 kg", shipTime: "2000-05-21-12:21", deliverTime: "1999-05-22-12:21" },
-        { from: "east", to: "west", size: "110inch^8inch", orderWeight: "1600kg", shipTime: "2015-05-21-12:21", deliverTime: "1999-05-25-12:21" },
-        { from: "japan", to: "korea", size: "90inch^7inch", orderWeight: "9 kg", shipTime: "2019-05-21-12:21", deliverTime: "1999-05-27-12:21" }],
-    };
 
     handleEmptyInput = () => {
-        this.setState({
-            error: "Please input your tracking number!"
-        })
+         this.setState({
+             error: "Please input your tracking number!"
+         })
     };
 
     render() {
+        const pendingData = this.props.orderHistory.pending.pending;
+        let lastPending=null;
+        if(pendingData.length!==0){
+            lastPending = pendingData[pendingData.length-1];
+        }
         return (
             <div>
                     <Content>
@@ -60,8 +56,7 @@ class User extends Component {
 
                                 <p className="tracking-text"> Tracking</p>
 
-                                <TrackingOnHome className="tracking-input" onEmptyInput = {this.handleEmptyInput}/>
-                                {/* <TrackingOnHome className="tracking-input"/> */}
+                                <TrackingOnHome className="tracking-input" onEmptyInput = {this.handleEmptyInput}/> 
 
 
                                 <Link to="/placeOrder">
@@ -95,7 +90,7 @@ class User extends Component {
                             <Col span={4}></Col>
                         </Row> */}
                         <div>
-                            {this.state.processing === null ? (
+                            {lastPending=== null ? (
                                 <div >
                                     <img src={homeimage} class="center"></img>
                                 </div>
@@ -104,13 +99,14 @@ class User extends Component {
                                     <a class="href">
                                         <img src={home_img} alt="logo" />
                                         <div className='recentorder'>
+                                            
                                             <List className='orderlist' size="small">
-                                                <p>Estimated: {this.state.processing[0].deliverTime} </p>
-                                                <p>From: {this.state.processing[0].from} </p>
-                                                <p>To: {this.state.processing[0].to} </p>
-                                                <p>Size: {this.state.processing[0].size} </p>
-                                                <p>Weight: {this.state.processing[0].weight}</p>
-                                                <p>Ship time: {this.state.processing[0].shipTime}</p>
+                                                <p>Estimated: {lastPending.arriveTime} </p>
+                                                <p>From: {lastPending.senderAddress} </p>
+                                                <p>To: {lastPending.receiverAddress} </p>
+                                                <p>Size: {lastPending.size} </p>
+                                                <p>Weight: {lastPending.weight}</p>
+                                                <p>Ship time: {lastPending.createTime}</p>
                                             </List>
                                         </div>
                                     </a>
